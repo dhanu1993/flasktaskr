da9fbbd57a0def4c85a2dd16bd67ffb4630ba61b
Dhanunjaya Panchireddy <dhanunjaya.panchireddy@gmail.com>
2017-12-15 16:16:17 +0530

    update

--- project/_config.py
+++ project/_config.py
@@ -8,5 +8,5 @@ SECRET_KEY = 'b\xfa\xe4\x94<\xd4wm\x84\x0f7\xe6)H\x0c\xe48\xedE\xcc^\xcaN?'
 DATABASE_PATH = os.path.join(basedir, DATABASE)
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///'+DATABASE_PATH
-DEBUG = True
+DEBUG = False
 SQLALCHEMY_TRACK_MODIFICATIONS = True
